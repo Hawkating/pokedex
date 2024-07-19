be156af723935d1d60ecae67b28b9cfad7c8057d
Jan B. <j.bruchwalski@web.de>
2024-07-20 00:59:10 +0200

    templates

--- js/script.js
+++ js/script.js
@@ -1,24 +1,44 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
+
 async function init(){
 let content = document.getElementById('content');
 content.innerHTML = initializePokedexTemplate();
-let pokemon = await loadPokemon("/pokemon/1");
+
+for (let i = 1; i<500; i++){
+let pokemon = await loadPokemon(`/pokemon/${i}`);
+
+ content.innerHTML += pokemon;
+
+//  let pokemonName = (`${BASE_URL}/pokemon/${i}["name"]`);
+// checkForBackgroundImage(`/pokemon/${i}`);
+
+}
 
 
 
-content.innerHTML += pokemon["name"];
 }
 
 async function loadPokemon(path=""){
-    // return new Promise ((resolve, reject) => {
-
-    // })
    let response = await fetch(BASE_URL + path);
-   let responseToString = await response.json();
-   console.log(responseToString);
-   return responseToString;
-
+   let responseToJson = await response.json();
+   let renderedCard = renderCard(responseToJson); 
    
+   return renderedCard;
 
 }
+
+// async function checkForBackgroundImage(path=""){
+//     let response = await fetch(BASE_URL + path);
+//     let responseToJson = await response.json();
+//     responseToJson.classList.add
+    //     if (responseToJson["types"][0]["type"]["name"] == "grass"){
+    //    document.getElementById(`pokemon-image-container-${responseToJson["name"]}`).classList.add('image-background-grass');
+    //     } 
+    //     if (responseToJson["types"][0]["type"]["name"] == "fire"){
+    //         document.getElementById(`pokemon-image-container-${responseToJson["name"]}`).classList.add('image-background-fire');
+    //          } 
+    //          if (responseToJson["types"][0]["type"]["name"] == "water"){
+    //             document.getElementById(`pokemon-image-container-${responseToJson["name"]}`).classList.add('image-background-water');
+    //              } 
+    // }
--- js/templates.js
+++ js/templates.js
@@ -1,8 +1,23 @@
 function initializePokedexTemplate(){
 return /*html*/`
     <header>
+        <img src="./img/logo.png">
+    </header>
+    <div id="pokemon-container"></div
+    `
+}
 
-    
-    </header>`
+function renderCard(responseAsJson){
+    console.log(responseAsJson);
+return /*html*/ `
+<div class="pokemon-card" id="pokemon-card-${responseAsJson['name']}">
+    <span><b>${responseAsJson["name"]}</b></span>
+    <div class="pokemon-image-container image-background-${responseAsJson['types'][0]['type']['name']}" id="pokemon-image-container-${responseAsJson['name']}">
+        <img class="pokemon-image" src="${responseAsJson["sprites"]["other"]["dream_world"]["front_default"]}">
+    </div>
+</div>
+`
+
 
 }
+

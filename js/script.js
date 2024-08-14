580c932ed26de2957af7a2ef80c1f8e0d7425d6f
Jan B. <j.bruchwalski@web.de>
2024-08-14 22:26:53 +0200

    loading spinner

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
+let EVOLUTION_URL = "";
 let currentPokemon = 0;
 let caught = [];
+let filtered = [];
+let evolutionChain = [];
+let myPokemonActive = false;
 
 async function init(){
+loadCaught();
 let content = document.getElementById('content');
 content.innerHTML = initializePokedexTemplate();
+showLoadingSpinner();
 let containerPokecard = document.getElementById('pokemon-container');
 document.getElementById('load-more-button').addEventListener('click', loadMorePokemons)
 for (let i = 1; i<26; i++){
    let pokemon = await loadPokemon(`/pokemon/${i}`);
    containerPokecard.innerHTML += pokemon;
-   currentPokemon ++;
+   renderTypesArea(`/pokemon/${i}`);
+   document.getElementById(`ability-info-${i}`).innerHTML = await loadAbility(`/pokemon/${i}`);
+         currentPokemon ++;
+      }
+      if (caught){
+      colorCaughtOnes();
    }
-// colorCaughtOnes();
+   hideLoadingSpinner();
 }
 
-// function colorCaughtOnes(){
-//    for (let i = 1; i < currentPokemon; i++){
-//    if (caught.includes(i)){
-//          document.getElementById(`caught-image-${i}`).classList.remove('grey');
-//    }
-//    }
-// }
+function colorCaughtOnes(){
+   for (let i = 0; i < caught.length; i++){
+   document.getElementById(`caught-image-${caught[i]}`).classList.remove('grey');
+}
+}
 
 
 function colorCaughtOnesActive(id){
@@ -36,21 +45,38 @@ async function loadPokemon(path=""){
    let response = await fetch(BASE_URL + path);
    let responseToJson = await response.json();
    let renderedCard = renderCard(responseToJson); 
-   
    return renderedCard;
-
 }
 
+async function loadAbility(path=""){
+   let response = await fetch(BASE_URL + path);
+   let responseToJson = await response.json();
+   let abilityInfo = await fetch (responseToJson["abilities"][0]["ability"]["url"]);
+   let abilityInfoToJson = await abilityInfo.json();
+   // let index = abilityInfoToJson["effect_entries"].findIndex() NOCHMAL DRAN WEIL DIE SPRACHE UNTERSCHIEDLICH IST
+
+   return abilityInfoToJson["effect_entries"][1]["short_effect"];
+   
+}
 
 async function loadMorePokemons(){
+   showLoadingSpinner();
    let containerPokecard = document.getElementById('pokemon-container');
    let currentPokemonPlus25 = currentPokemon + 25;
    for (let i = currentPokemon + 1; i <= currentPokemonPlus25; i++){
       let pokemon = await loadPokemon(`/pokemon/${i}`);
       containerPokecard.innerHTML += pokemon;
       currentPokemon ++;
+      renderTypesArea(`/pokemon/${i}`);
+      document.getElementById(`ability-info-${i}`).innerHTML = await loadAbility(`/pokemon/${i}`);
       }
+      if (caught){
+         colorCaughtOnes();
+      }
+      hideLoadingSpinner();
+      document.getElementById('load-more-button').classList.remove('d-none');
 }
+
 
 function playCry(forCry){
    console.log(forCry);
@@ -63,8 +89,149 @@ function catchPokemon(id){
    if (caught.includes(id)){
       let indexOfId = caught.indexOf(id);
       caught.splice(indexOfId, 1)
+      if(myPokemonActive){
+         showCaughtOnes();
+      }
    } else {
    caught.push(id);
    }
+   if (!myPokemonActive){
    colorCaughtOnesActive(id);
+   saveCaught();
+}
+}
+
+function saveCaught(){
+   let caughtAsText = JSON.stringify(caught);
+   localStorage.setItem('caught', caughtAsText);
+}
+
+function loadCaught(){
+   let caughtToString = localStorage.getItem('caught');
+   if (caughtToString){
+   let caughtToArray = JSON.parse(caughtToString);
+   for (let i = 0; i < caughtToArray.length; i++){
+      caught.push(caughtToArray[i]);
+   }
+   }
+}
+
+
+async function showCaughtOnes(){
+   myPokemonActive = true;
+   document.getElementById('showMyPokemon').classList.add('d-none');
+   document.getElementById('showAll').classList.remove('d-none');
+   let containerPokecard = document.getElementById('pokemon-container');
+   containerPokecard.innerHTML = ``;
+   currentPokemon = 0;
+   for (let i = 0; i<caught.length; i++){
+      let pokemon = await loadPokemon(`/pokemon/${caught[i]}`);
+      containerPokecard.innerHTML += pokemon;
+      renderTypesArea(`/pokemon/${caught[i]}`);
+      }
+      
+         if (caught){
+         colorCaughtOnes();
+      }
+      document.getElementById('load-more-button').classList.add('d-none');
+}
+
+async function filterPokemon(){
+   showLoadingSpinner();
+   document.getElementById('showMyPokemon').classList.add('d-none');
+   document.getElementById('showAll').classList.remove('d-none');
+   let containerPokecard = document.getElementById('pokemon-container');
+   containerPokecard.innerHTML = ``;
+   pushFiltered();
+   
+   }
+
+  async function renderContainerPokeCards(){
+      let containerPokecard = document.getElementById('pokemon-container');
+      let pokemon = await loadPokemon(`/pokemon/${filtered[0]}`);
+      containerPokecard.innerHTML += pokemon;
+      renderTypesArea(`/pokemon/${filtered[0]}`);
+   }
+
+
+async function pushFiltered(){
+   filtered = [];
+   let forFilter = document.getElementById('inputfield-filter');
+   for(let i = 1; i<250; i++){
+      let response = await fetch(BASE_URL + `/pokemon/${i}`);
+      let responseToJson = await response.json();
+
+      if(forFilter.value == i || forFilter.value == responseToJson['name']){
+         filtered.push(i);
+      }
+   }
+   document.getElementById('load-more-button').classList.add('d-none');
+   renderContainerPokeCards();
+   forFilter.value = ``;
+   hideLoadingSpinner();
+}
+
+async function showEvolutionChain(id){
+  
+let evolutionArea = document.getElementById('evolution-area');
+loadEvolutionChain(id);
+// for (let i = 0; i< evolutionChain.length; i++){
+
+
+// }
+}
+
+async function loadEvolutionChain(path="") {
+   
+   let response = await fetch(BASE_URL + "pokemon-species/" + path);
+   let responseToJson = await response.json();
+   let evolution = await fetch (responseToJson["evolution_chain"]["url"]);
+   let evolutionToJson = await evolution.json();
+   let evolution2 = await fetch (BASE_URL + "evolution-chain/" + evolutionToJson["id"]);
+   let evolution2ToJson = await evolution2.json();
+   renderEvolutionChain(evolution2ToJson.chain.species.name, evolution2ToJson.chain.evolves_to[0].species.name, evolution2ToJson.chain.evolves_to[0].evolves_to[0].species.name);
+   // console.log(evolution2ToJson.chain.species.name);
+   // console.log(evolution2ToJson.chain.evolves_to[0].species.name);
+   // console.log(evolution2ToJson.chain.evolves_to[0].evolves_to[0].species.name);
+   
+}
+
+function renderEvolutionChain(name1, name2, name3){
+   let evolutionArea = document.getElementById('evolution-area');
+
+   document.getElementById('dialog-background').classList.remove('d-none');
+   evolutionArea.innerHTML = `${name1}, ${name2}, ${name3}`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function closeChain(){
+   document.getElementById('dialog-background').classList.add('d-none');
+}
+
+function showLoadingSpinner(){
+   document.getElementById('dialog-background-spinner').classList.remove('d-none');
+}
+
+function hideLoadingSpinner(){
+   document.getElementById('dialog-background-spinner').classList.add('d-none');
+}
+
+function showAll(){
+   myPokemonActive = false;
+   document.getElementById('showMyPokemon').classList.remove('d-none');
+   document.getElementById('showAll').classList.add('d-none');
+   currentPokemon = 0;
+   document.getElementById('pokemon-container').innerHTML = ``;
+   loadMorePokemons();
 }
--- js/templates.js
+++ js/templates.js
@@ -1,13 +1,24 @@
 function initializePokedexTemplate(){
 return /*html*/`
+<div id="dialog-background" class="dialog-bg d-none">
+    
+    <div id="evolution-area">
+    </div>
+    <div class="close-chain" onclick="closeChain()">close evolution-chain</div>
+</div>
+<div id="dialog-background-spinner" class="dialog-bg d-none">
+    <img class="loading-spinner rotate" src="./img/pokeball.png">
+    <span>loading...</span>
+</div>
+
     <header>
         <img src="./img/logo.png">
     </header>
-    <section>
-    <div>search</div>
+    <section class="filter-section">
+    <div><input id="inputfield-filter" placeholder="ID or name"><button onclick="filterPokemon()">search</button></div><div onclick="showCaughtOnes()" class="" id="showMyPokemon">show my Pokèmon</div><div onclick="showAll()" class="d-none" id="showAll">show all</div>
     <div id="pokemon-container"></div>
     </section>
-    <div><button id="load-more-button">load more</button></div>
+    <div class="load-more-area"><button id="load-more-button">load more</button></div>
     `
 }
 
@@ -15,15 +26,114 @@ function renderCard(responseAsJson){
 return /*html*/ `
 <div class="pokemon-card card-background-${responseAsJson['types'][0]['type']['name']}" id="pokemon-card-${responseAsJson['name']}">
     <div class="header-for-card">
-        <span><b>${responseAsJson["name"]}</b></span><span><b>#${responseAsJson["id"]}</b></span>
+        <h2><b>${responseAsJson["name"]}</b></h2><span><b>#${responseAsJson["id"]}</b></span>
     </div>
     <div class="pokemon-image-container image-background-${responseAsJson['types'][0]['type']['name']}" id="pokemon-image-container-${responseAsJson['name']}">
         <img onclick="playCry('${responseAsJson['cries']['latest']}')" class="pokemon-image" src="${responseAsJson["sprites"]["other"]["dream_world"]["front_default"]}">
     </div>
-    <div>caught? <img id="caught-image-${responseAsJson["id"]}" onclick="catchPokemon('${responseAsJson["id"]}')" class="caught-image grey" src="./img/pokeball.png"></div>
+    <div class="types-area" id="types-area-${responseAsJson['id']}"></div>
+    <div class="weight-height"><span>weight: ${responseAsJson["weight"]}</span><span>height: ${responseAsJson["height"]}</span></div>
+    <div class="card-ability-area">
+    <div><span><b>${responseAsJson["abilities"][0]["ability"]["name"]}:</b></span><p id="ability-info-${responseAsJson["id"]}"></p></div>
+    </div>
+    <div class="card-bottom-area">
+     <div class="evolution-chain hidden" onclick="showEvolutionChain(${responseAsJson['id']})">show evolution-chain</div>       <div class="catch-area"><b>caught:</b> <img id="caught-image-${responseAsJson["id"]}" onclick="catchPokemon('${responseAsJson["id"]}')" class="caught-image grey" src="./img/pokeball.png"></div>
+    </div>
 </div>
 `
-
-
 }
 
+async function renderTypesArea(path=""){
+    let response = await fetch(BASE_URL + path);
+    let responseToJson = await response.json();
+    
+    for (let i = 0; i<responseToJson['types'].length; i++){
+    document.getElementById(`types-area-${responseToJson['id']}`).innerHTML +=  /*html*/ `
+    <img class="type-pic" src="./img/type_${responseToJson['types'][i]['type']['name']}.png">
+    `;
+    }
+}
+
+
+
+// {
+//     "baby_trigger_item": null,
+//     "chain": {
+//       "evolution_details": [],
+//       "evolves_to": [
+//         {
+//           "evolution_details": [
+//             {
+//               "gender": null,
+//               "held_item": null,
+//               "item": null,
+//               "known_move": null,
+//               "known_move_type": null,
+//               "location": null,
+//               "min_affection": null,
+//               "min_beauty": null,
+//               "min_happiness": null,
+//               "min_level": 16,
+//               "needs_overworld_rain": false,
+//               "party_species": null,
+//               "party_type": null,
+//               "relative_physical_stats": null,
+//               "time_of_day": "",
+//               "trade_species": null,
+//               "trigger": {
+//                 "name": "level-up",
+//                 "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
+//               },
+//               "turn_upside_down": false
+//             }
+//           ],
+//           "evolves_to": [
+//             {
+//               "evolution_details": [
+//                 {
+//                   "gender": null,
+//                   "held_item": null,
+//                   "item": null,
+//                   "known_move": null,
+//                   "known_move_type": null,
+//                   "location": null,
+//                   "min_affection": null,
+//                   "min_beauty": null,
+//                   "min_happiness": null,
+//                   "min_level": 36,
+//                   "needs_overworld_rain": false,
+//                   "party_species": null,
+//                   "party_type": null,
+//                   "relative_physical_stats": null,
+//                   "time_of_day": "",
+//                   "trade_species": null,
+//                   "trigger": {
+//                     "name": "level-up",
+//                     "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
+//                   },
+//                   "turn_upside_down": false
+//                 }
+//               ],
+//               "evolves_to": [],
+//               "is_baby": false,
+//               "species": {
+//                 "name": "charizard",
+//                 "url": "https://pokeapi.co/api/v2/pokemon-species/6/"
+//               }
+//             }
+//           ],
+//           "is_baby": false,
+//           "species": {
+//             "name": "charmeleon",
+//             "url": "https://pokeapi.co/api/v2/pokemon-species/5/"
+//           }
+//         }
+//       ],
+//       "is_baby": false,
+//       "species": {
+//         "name": "charmander",
+//         "url": "https://pokeapi.co/api/v2/pokemon-species/4/"
+//       }
+//     },
+//     "id": 2
+//   }
